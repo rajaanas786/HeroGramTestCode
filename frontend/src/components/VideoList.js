6666e67cf77fb2630c4c396e049cb377bf0fac88
Anas <apple@APPLEs-MacBook-Pro.local>
2024-12-10 18:46:25 +0400

    new

--- frontend/src/components/VideoList.js
+++ frontend/src/components/VideoList.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import ReactPlayer from 'react-player';
 
 
 
@@ -12,7 +11,7 @@ const VideoList = () => {
   useEffect(() => {
     const fetchVideos = async () => {
       try {
-        const response = await axios.get('http://api/videos/list');
+        const response = await axios.get('http://159.65.122.248:5000/api/videos/list');
         setVideos(response.data); // Update state with the fetched videos
         
       } catch (err) {
